ba65974e16fa62219349617c9c69c67692597351
vicmgs <vic.mgs@gmail.com>
2016-09-26 15:03:09 -0700

    Make basic app tests pass

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,41 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/messages',
+  init: function() {
+
+  },
+  send: function(message) {
+    $.ajax({
+      url: this.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      url: this.server,
+      type: 'GET',
+      success: function (data) {
+        console.log('chatterbox: Message received', data);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to receive message', data);
+      }
+    });
+  },
+  clearMessages: function() {
+    $('#chats').html('');
+  },
+  renderMessage: function(message) {
+    $('#chats').append(`<p>${message.text}</p>`);
+  },
+  renderRoom: function(room) {
+    $('#roomSelect').append(`<h1>${room}</h1>`);
+  }
+};
