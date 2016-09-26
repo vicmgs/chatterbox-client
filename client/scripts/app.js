f7c2c29aabb0c9ef6971b8a82860ae8e4c5ea451
vicmgs <vic.mgs@gmail.com>
2016-09-26 16:30:28 -0700

    Pass all tests for chatterbox, still have small mocha test issue

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,26 @@
 var app = {
   server: 'https://api.parse.com/1/classes/messages',
   init: function() {
+    $('.submit').on('click submit', this.handleSubmit.bind(this));
+    // this = app
+    // $('#main').append('<div class="userList"></div>');
+    this.fetch(function (data) {
+      var users = {};
+      data.results.forEach(function(datum) {
+        this.renderMessage(datum);
+        users[datum.username] = true;
 
+      }.bind(this));
+
+      // render each user
+      for (var key in users) {
+        this.renderUser(key);
+      }
+    }.bind(this));
+
+    // $(document.body).on('click', '.username', function() {
+    //   console.log('haha');
+    // });
   },
   send: function(message) {
     $.ajax({
@@ -17,25 +36,54 @@ var app = {
       }
     });
   },
-  fetch: function() {
+  fetch: function(callback) {
     $.ajax({
       url: this.server,
       type: 'GET',
-      success: function (data) {
-        console.log('chatterbox: Message received', data);
-      },
-      error: function (data) {
-        console.error('chatterbox: Failed to receive message', data);
-      }
+      success: callback,
+      error: callback
     });
   },
   clearMessages: function() {
     $('#chats').html('');
   },
   renderMessage: function(message) {
-    $('#chats').append(`<p>${message.text}</p>`);
+    var $newMessage = $(`<p><span class='username' data-username='${message.username}'>${message.text}</span></p>`);
+    $('#chats').append($newMessage);
+    var context = this;
+    $newMessage.on('click', '.username', function() {
+      var username = $(this).data('username');
+      context.handleUsernameClick(username);
+    });
   },
   renderRoom: function(room) {
     $('#roomSelect').append(`<h1>${room}</h1>`);
+  },
+  renderUser: function(username) {
+    var context = this;
+    var $newUser = $(`<p class='username' data-username='${username}'>${username}</p>`);
+    $newUser.appendTo('#userList');
+    $newUser.on('click', function() {
+      var username = $(this).data('username');
+      context.handleUsernameClick(username);
+    });
+
+  },
+  handleUsernameClick: function(friend) {
+    $('#friends').append(`<h2>${friend}</h1>`);
+  },
+  handleSubmit: function() {
+    var message = {
+      text: $('#message').val(),
+      username: 'John the other guy, jk not me',
+      roomname: 'Do not disturb'
+    };
+    console.log(message);
+    this.send(message);
+
   }
 };
+
+$(document).ready(function() {
+  app.init();
+});
